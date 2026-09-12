c44b50d62033a6cb12613f6ae3f0ee6847a0352c
Sumaiya-khanam06 <sumaiyakhanamsummy@gmail.com>
2026-09-12 10:46:42 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 1445533
+let accountEmail = "sumiaya@example.com"
+let accountPassword = "sumaiya123"
+let accountCity = "Banglore"
+let accountState;
+//accountId = 1234567
+accountEmail = "Abiya@example.com"
+accountPassword = "Abiya123"
+accountCity = "jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail ,accountPassword,accountCity,accountState]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
