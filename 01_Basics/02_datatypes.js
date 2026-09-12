427164e224903e3a51de97596e40ee5f6c0f0d02
Sumaiya-khanam06 <sumaiyakhanamsummy@gmail.com>
2026-09-12 11:04:06 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";// treat all JS code as newer version
+
+//alert( 3 + 3) // we are using nodejs,not browser
+
+console.log(3
+    +
+    3)// its not a good practice to write code in multiple lines, but it is possible,code readbility is very important
+
+let name = "Sumiaya"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint => larger than 2 to power 53
+//string => any value inside double or single quotes""
+// boolean => true or false
+//null => empty value/standalone value
+//undefined => value is not assigned
+//symbol => unique value which cannot be changed
+
+
+//object => key value pair, datatypes which can hold multiple values
+
+console.log(typeof null)
+console.log(typeof state)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof 10n)
