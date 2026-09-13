5fe36edf4e1d95662ed772b95c1012bb960a9fe7
Sumaiya-khanam06 <sumaiyakhanamsummy@gmail.com>
2026-09-13 15:41:10 +0530

    operations.js

---
+++ 01_Basics/operations.js
@@ -0,0 +1,43 @@
+let value = 3
+let negValue = -value
+console.log(negValue); // -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2); // remainder
+// console.log(2**2); // 2 power 2 => 4
+// console.log(2**3); // 2 power 3 => 8
+
+let str1="hello"
+let str2=" summi"
+
+let str3 = str1 +  str2
+console.log(str3); // hello summi
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+console.log("1" + 2 + 2 + 2); // 1222
+console.log((3+4)*5%3); // 2
+
+console .log(3+4*5%3); // 4
+
+console.log(+true);
+console.log(+false);
+console.log(+"");
+
+//prefix and postfix js mdn search in google and study it
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // 101
+
+let newGameCounter = 100;
+++newGameCounter;
+console.log(newGameCounter); // 101
+
+//link to study
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
